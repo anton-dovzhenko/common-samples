fd7260cab830211c43a0755eeac37e27dcb7e7ee
anthony300787@gmail.com <anthony300787@gmail.com@339dd69e-32b6-bbf2-138d-d0c1c624e4ae>
2014-07-20 08:47:28 +0000

    Atmosphere chat example + Atmosphere web socket implementation.

--- trunk/web-streaming/ping-pong/src/main/java/com/gammadevs/pingpong/model/Player.java
+++ trunk/web-streaming/ping-pong/src/main/java/com/gammadevs/pingpong/model/Player.java
@@ -5,9 +5,9 @@ package com.gammadevs.pingpong.model;
  */
 public class Player {
 
-    private int height;
-    private int width;
-    private int padding;
+    private final int height;
+    private final int width;
+    private final int padding;
     private int left;
 
     public Player(int height, int width, int padding, int left) {
@@ -21,24 +21,12 @@ public class Player {
         return height;
     }
 
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
     public int getWidth() {
         return width;
     }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
     public int getPadding() {
         return padding;
-    }
-
-    public void setPadding(int padding) {
-        this.padding = padding;
     }
 
     public int getLeft() {
