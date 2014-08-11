04a21f8ff24fca45829d38f78f84262684f8e2cf
anthony300787@gmail.com <anthony300787@gmail.com@339dd69e-32b6-bbf2-138d-d0c1c624e4ae>
2014-08-11 15:29:28 +0000

    R samples. ODBC connections.

--- trunk/R/database_connection/odbc.R
+++ trunk/R/database_connection/odbc.R
@@ -1,3 +1,6 @@
+## 1. Install ODBC for reqiured database
+## 2. Add users DSN (see http://stackoverflow.com/questions/6796252/setting-up-postgresql-odbc-on-windows) 
+
 ## install.packages("RODBC")
 
 library(RODBC)
