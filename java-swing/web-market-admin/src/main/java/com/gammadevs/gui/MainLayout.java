d79e6a3224a8052605a611ba2fdfb920ceb0f36b
anthony300787@gmail.com <anthony300787@gmail.com@339dd69e-32b6-bbf2-138d-d0c1c624e4ae>
2014-09-30 15:06:40 +0000

    Swing example. Category Tab

--- java-swing/web-market-admin/src/main/java/com/gammadevs/gui/MainLayout.java
+++ java-swing/web-market-admin/src/main/java/com/gammadevs/gui/MainLayout.java
@@ -1,5 +1,6 @@
 package com.gammadevs.gui;
 
+import com.gammadevs.gui.category.CategoryTabPanel;
 import com.gammadevs.gui.producer.ProducerTabPanel;
 
 import javax.swing.*;
@@ -12,7 +13,7 @@ public class MainLayout extends JTabbedPane {
 
     private final Dimension size = new Dimension(500, 400);
 
-    private final JLabel categoryPanel = new JLabel("Category");
+    private final CategoryTabPanel categoryPanel = new CategoryTabPanel();
     private final JLabel commodityPanel = new JLabel("Commodity");
     private final ProducerTabPanel producerPanel = new ProducerTabPanel();
 
--- java-swing/web-market-admin/src/main/java/com/gammadevs/model/Category.java
+++ java-swing/web-market-admin/src/main/java/com/gammadevs/model/Category.java
@@ -3,33 +3,33 @@ package com.gammadevs.model;
 /**
  * Created by Anton on 9/14/2014.
  */
-public class Category {
+public class Category implements Comparable<Category> {
 
-    private long id;
-    private long parentId;
-    private String name;
+    private final Long id;
+    private final Long parentId;
+    private final String name;
 
-    public long getId() {
+    public Category(Long id, Long parentId, String name) {
+        this.id = id;
+        this.parentId = parentId;
+        this.name = name;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public long getParentId() {
+    public Long getParentId() {
         return parentId;
-    }
-
-    public void setParentId(long parentId) {
-        this.parentId = parentId;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    @Override
+    public int compareTo(Category o) {
+        return name.compareTo(o.name);
     }
+
 }
