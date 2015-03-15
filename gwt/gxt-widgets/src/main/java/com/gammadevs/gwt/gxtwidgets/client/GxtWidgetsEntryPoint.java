c4eae5869b1796e4202240b10fdef0bfadf31ac4
anthony300787@gmail.com <anthony300787@gmail.com@339dd69e-32b6-bbf2-138d-d0c1c624e4ae>
2015-03-15 12:55:14 +0000

    GXT-Widgets: Frozen Grid.

--- gwt/gxt-widgets/src/main/java/com/gammadevs/gwt/gxtwidgets/client/GxtWidgetsEntryPoint.java
+++ gwt/gxt-widgets/src/main/java/com/gammadevs/gwt/gxtwidgets/client/GxtWidgetsEntryPoint.java
@@ -1,17 +1,14 @@
 package com.gammadevs.gwt.gxtwidgets.client;
 
-import com.gammadevs.gwt.gxtwidgets.client.examples.FrozenGridExample;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 
-/**
- * Created by Anton on 3/9/2015.
- */
 public class GxtWidgetsEntryPoint implements EntryPoint {
 
     public void onModuleLoad() {
         GxtWidgetsMainLayout layout = new GxtWidgetsMainLayout();
         RootLayoutPanel.get().add(layout);
+
     }
 
 }
